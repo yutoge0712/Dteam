017cf51fea06c632e4023d84b2af3426642c26d0
peko-dd <peko7japan@gmail.com>
2022-09-16 15:13:21 +0900

    aa

---
+++ Project/GameProject/Game/ClickPoint.cpp
@@ -0,0 +1,25 @@
+#include"ClickPoint.h"
+#include "../Base/Base.h"
+#include"ShareNum.h"
+
+ClickPoint::ClickPoint()
+	:Base(eType_ClickPoint)
+{
+
+}
+
+void ClickPoint::Update()
+{
+	if (PUSH(CInput::eMouseL)) {
+		ShareNum::score += 1500;
+
+	}
+}
+
+void ClickPoint::Draw()
+{
+	m_img.Draw();
+}
+
+
+
--- Project/GameProject/Game/ClickPoint.h
+++ Project/GameProject/Game/ClickPoint.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "../Base/Base.h"
+class ClickPoint : public Base {
+public:
+	CImage m_img;
+	ClickPoint();
+	void Update();
+	void Draw();
+
+};
